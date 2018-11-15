02ca118d006475fd49251eeb5a726e17449e67a1
Robin Gruenke <robingruenke@Robins-MacBook-Pro.local>
2018-11-15 19:28:42 +0100

    inital commit

---
+++ limefood/README.md
@@ -0,0 +1,6 @@
+# Limefood 🍋
+Inserts new linefeeds (drops the existing) to create sentences with given length.
+
+```
+linefood --path ~/test.txt --length 80
+```
---
+++ limefood/src/main.rs
@@ -0,0 +1,47 @@
+#[macro_use] extern crate structopt;
+extern crate failure;
+extern crate exitfailure;
+
+use std::{
+    fs,
+    path::{PathBuf},
+};
+
+use structopt::StructOpt;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+
+// Our CLI arguments. (help and version are automatically generated)
+// Documentation on how to use:
+// https://docs.rs/structopt/0.2.10/structopt/index.html#how-to-derivestructopt
+#[derive(StructOpt)]
+struct Cli {
+    // The path of the file we want to look at.
+    #[structopt(short = "p", long = "path", parse(from_os_str))]
+    path: PathBuf,
+    #[structopt(short = "l", long = "length")]
+    length: usize
+}
+
+fn main() -> Result<(), ExitFailure>{
+    let cli = Cli::from_args();
+    let Cli { path, length } = &cli;
+
+    let contents = fs::read_to_string(path)
+        .with_context(|_| format!("Could not read file {}", path.to_str().unwrap()))?;
+
+    let mut out = String::new();
+    let chars = contents.chars().enumerate();
+
+    for (i, char_item) in chars {
+        if char_item != '\n' {
+            out.push(char_item);
+        }
+        if i > 0 && i % length == 0 {
+            out.push('\n');
+        }
+    }
+    fs::write(path, out)?;
+    Ok(())
+}
+
